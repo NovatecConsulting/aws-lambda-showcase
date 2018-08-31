ff2af324b164b5ed9a61788881fecbbcbaa42f23
Timo Freiberg <timo.freiberg@novatec-gmbh.de>
2018-08-31 11:09:37 +0200

    Add argument to function

--- src/main/kotlin/novatec/Hello.kt
+++ src/main/kotlin/novatec/Hello.kt
@@ -2,7 +2,11 @@ package novatec
 
 class Hello {
 
-    fun handleRequest() = "Thanks, Travis!"
+    fun handleRequest(name: String = "") = if (name.isNotBlank()) {
+        "Goodbye $name"
+    } else {
+        "Goodbye everyone"
+    }
 
 
 }
